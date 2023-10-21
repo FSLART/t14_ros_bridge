9d8de41facb8f7e9b2e692059dce2d89857db8b8
carlostojal <carlos.tojal@hotmail.com>
2023-10-21 19:36:09 +0100

    implement data extraction function

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ from can import Message
 import json
 
 # load IDs from a file
-def load_config(path: str = "docs/ids.json") -> dict:
+def load_config(path: str = "docs/ids.json"):
 
     # open the file
     try:
@@ -14,12 +14,20 @@ def load_config(path: str = "docs/ids.json") -> dict:
     raw_json = f.read()
 
     # parse the JSON
+    # relates variable names to CAN IDs and bytes
     try:
-        config = json.loads(raw_json)
+        var_to_can = json.loads(raw_json)
     except:
         raise ValueError("Invalid JSON file encoding!")
     
-    return config
+    id_to_vars = {}
+    for var in var_to_can:
+        if not var_to_can[var]['ID'] in id_to_vars:
+            id_to_vars[var_to_can[var]['ID']] = [var]
+        else:
+            id_to_vars[var_to_can[var]['ID']].append(var)
+
+    return var_to_can, id_to_vars
 
 
 # extract a value from a message
@@ -40,3 +48,23 @@ def extract_value_from_msg(message: Message, byte_range: tuple) -> int:
         value += message.data[i] << (8 * (byte_range[1] - i))
 
     return value
+
+# handle an incoming message
+# receives the message, a mapping of IDs to variable names, and a mapping of variable names to CAN IDs and bytes
+def handle_message(message: Message, id_to_vars: dict, var_to_can: dict):
+    
+    # if the ID is not used by any variable, discard it
+    if not hex(message.arbitration_id) in id_to_vars:
+        return
+    
+    # get the variable names that use this ID
+    variables = id_to_vars[hex(message.arbitration_id)]
+
+    # extract each of the variables to a list
+    values = []
+    for var in variables:
+        new_val = {
+            'name': var,
+            'value': extract_value_from_msg(message, tuple(var_to_can[var]['bytes']))
+        }
+        values.append(new_val)
